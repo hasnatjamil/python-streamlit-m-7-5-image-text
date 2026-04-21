914cedf552c225be7b8ec90208fd44ed7cdb24f7
hasnatjamil <139052903+hasnatjamil@users.noreply.github.com>
2026-04-21 13:59:27 +0600

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ if pressed:
                     #        border-radius:6px;
                     #        cursor:pointer;"
                            >
-                # 📋 Copy Text
+                #Copy Text
             </button>
 
            """
